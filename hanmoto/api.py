3bf4ce18e454e914b1b36481f8e083bf4ffe113b
kush <kouta528bado@yahoo.co.jp>
2023-02-24 14:50:55 +0000

    add single image print feature to api

--- hanmoto/api.py
+++ hanmoto/api.py
@@ -1,5 +1,7 @@
+import base64
 from abc import abstractmethod
 from enum import Enum
+from io import BytesIO
 from typing import Dict, List
 
 from dotenv import load_dotenv
@@ -22,24 +24,22 @@ class PrintableModel(BaseModel):
 
 
 class TextStyle(BaseModel):
-    align: str
-    font: int
-    bold: bool
-    underline: int
-    width: int
-    height: int
-    density: int
-    invert: bool
-    flip: bool
-    double_width: int
-    double_height: int
+    align: str = "left"
+    font: int = 0
+    bold: bool = False
+    underline: int = 0
+    width: int = 1
+    height: int = 1
+    density: int = 8
+    invert: bool = False
+    flip: bool = False
+    double_width: bool = False
+    double_height: bool = False
 
 
 class TextModel(PrintableModel):
     content: str
-    style: TextStyle = TextStyle(
-        invert=False, flip=False, bold=False, underline=0
-    )
+    style: TextStyle = TextStyle()
 
     def to_hmt(self) -> Printable:
         return HmtText(self.content, properties=self.style.dict())
@@ -66,22 +66,40 @@ class ImageModel(PrintableModel):
 
     def to_hmt(self) -> Printable:
         if self.base64:
-            image = Image.open(self.base64)
+            img_base64_bytes = base64.b64decode(self.base64)
+            image = Image.open(BytesIO(img_base64_bytes))
         elif self.image_src:
             image = self.image_src
+        else:
+            raise Exception("")
         return HmtImage(image, properties=self.style.dict())
 
 
 class Sequence(BaseModel):
-    contents: List[Printable]
+    contents: List[PrintableModel]
 
 
-@app.post("/print")
-def print_text(sequence: Sequence) -> Dict[str, str]:
+@app.post("/print/sequence")
+def print_sequence(sequence: Sequence) -> Dict[str, str]:
     with printer:
-        for elem in sequence:
-            properties = elem.style.dict()
-            texts = [HmtText(text.content, properties=properties)]
-            printer.print_sequence(texts)
+        printables = [elem.to_hmt() for elem in sequence.contents]
+        printer.print_sequence(printables)
+
+    return {"status": "success"}
+
+
+@app.post("/print/text")
+def print_text(text: TextModel) -> Dict[str, str]:
+    with printer:
+        printer.print_sequence([text.to_hmt()])
+
+    return {"status": "success"}
+
+
+@app.post("/print/image")
+def print_image(image: ImageModel) -> Dict[str, str]:
+    print(image)
+    with printer:
+        printer.print_sequence([image.to_hmt()])
 
     return {"status": "success"}
